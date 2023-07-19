733f21429ea0769ca856c3660068a22a36473361
Shailu <shailesh.respond@gmail.com>
2023-07-19 20:09:44 +0530

    fifth

--- Form/index.js
+++ Form/index.js
@@ -25,15 +25,20 @@ function storeData(event) {
   const deserialise_value = JSON.parse(localStorage.getItem(key));
   const newList = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.className = "btn btn-info mb-2";
+  const editBtn = document.createElement("button");
+  delBtn.className = "btn btn-info mb-2 mr-2";
+  editBtn.className = "btn btn-info mb-2 ml-2";
   delBtn.id = "del";
   delBtn.addEventListener("click", deleteItem);
+  editBtn.addEventListener("click", editItem);
   delBtn.appendChild(document.createTextNode("delete"));
+  editBtn.appendChild(document.createTextNode("edit"));
 
   const text = `Name: ${deserialise_value.name}, Email: ${deserialise_value.email}, Phone: ${deserialise_value.phone}`;
 
   newList.appendChild(document.createTextNode(text));
   newList.appendChild(delBtn);
+  newList.appendChild(editBtn);
   listItem.appendChild(newList);
 }
 
@@ -43,6 +48,30 @@ function deleteItem(event) {
     .split(",")[1]
     .trim()
     .slice(7);
+
+  const itemToRemove = document.getElementsByTagName("li");
+  Array.from(itemToRemove).forEach((element) => {
+    if (element.textContent.includes(emailToDelete)) {
+      listItem.removeChild(element);
+      localStorage.removeItem(emailToDelete);
+    }
+  });
+}
+function editItem(event) {
+  event.preventDefault();
+  const emailToDelete = event.target.parentElement.textContent
+    .split(",")[1]
+    .trim()
+    .slice(7);
+  const nameToEdit = event.target.parentElement.textContent
+    .split(",")[0]
+    .trim()
+    .slice(6);
+  const editItem = JSON.parse(localStorage.getItem(nameToEdit));
+  email.value = editItem.email;
+  phone.value = editItem.phone;
+  time.value = editItem.time;
+  date.value = editItem.date;
 
   const itemToRemove = document.getElementsByTagName("li");
   Array.from(itemToRemove).forEach((element) => {
