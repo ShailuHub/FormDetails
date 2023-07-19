4391e3689ca85a553898ff96f7374ad47149b510
Shailu <shailesh.respond@gmail.com>
2023-07-19 18:15:57 +0530

    fourth

--- Form/index.js
+++ Form/index.js
@@ -1,29 +1,54 @@
 const form = document.getElementById("addForm");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+const listItem = document.querySelector(".allItems");
 
 form.addEventListener("submit", storeData);
 
 function storeData(event) {
   event.preventDefault();
   // Get the input field values
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  const date = document.getElementById("date").value;
-  const time = document.getElementById("time").value;
+
   const detail = {
-    name: name,
-    email: email,
-    phone: phone,
-    date: date,
-    time: time,
+    name: name.value,
+    email: email.value,
+    phone: phone.value,
+    date: date.value,
+    time: time.value,
   };
   const key = detail.name;
   const value = JSON.stringify(detail);
   localStorage.setItem(key, value);
   const deserialise_value = JSON.parse(localStorage.getItem(key));
-  const listItem = document.querySelector(".allItems");
   const newList = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.className = "btn btn-info mb-2";
+  delBtn.id = "del";
+  delBtn.addEventListener("click", deleteItem);
+  delBtn.appendChild(document.createTextNode("delete"));
+
   const text = `Name: ${deserialise_value.name}, Email: ${deserialise_value.email}, Phone: ${deserialise_value.phone}`;
+
   newList.appendChild(document.createTextNode(text));
+  newList.appendChild(delBtn);
   listItem.appendChild(newList);
+}
+
+function deleteItem(event) {
+  event.preventDefault();
+  const emailToDelete = event.target.parentElement.textContent
+    .split(",")[1]
+    .trim()
+    .slice(7);
+
+  const itemToRemove = document.getElementsByTagName("li");
+  Array.from(itemToRemove).forEach((element) => {
+    if (element.textContent.includes(emailToDelete)) {
+      listItem.removeChild(element);
+      localStorage.removeItem(emailToDelete);
+    }
+  });
 }
